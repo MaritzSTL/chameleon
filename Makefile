c6f07dc872b41927cfc7cda153094f29ef3d4777
Ryan Coleman <me@ryuhhnn.com>
2019-08-15 15:43:39 -0500

    adds the chameleon-card component

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ link_deps:
 	cd packages/button && npm link @chameleon-ds/theme && cd ../..
 	cd packages/input && npm link @chameleon-ds/theme && cd ../..
 	cd packages/textarea && npm link @chameleon-ds/theme && cd ../..
+	cd packages/card && npm link @chameleon-ds/theme && cd ../..
---
+++ packages/theme/base/card/index.ts
@@ -0,0 +1,15 @@
+import { css } from "lit-element";
+
+export default css`
+  :host {
+    background-color: var(--color-surface);
+    border: 1px solid var(--color-gray-lightest);
+    display: inline-block;
+    padding: 10px;
+  }
+
+  .chameleon-card {
+    background-color: var(--color-surface);
+    border: 1px solid var(--gray-lightest);
+  }
+`;
