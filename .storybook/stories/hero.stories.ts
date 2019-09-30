c212dddfa2f9e65e9b3cf38d7e032b480f93d208
Noel Hwande <noel.hwande@maritz.com>
2019-09-30 11:22:19 -0500

    Adds separate examples for gradient and image
    Closes #64

--- .storybook/stories/hero.stories.ts
+++ .storybook/stories/hero.stories.ts
@@ -9,26 +9,32 @@ const stories = storiesOf("Hero", module);
 // to this and make these types compatible.
 stories.addDecorator(withKnobs as any);
 
-stories.addParameters({
-  backgrounds: [
-    { name: "gray", value: "#f5f5f8", default: true },
-    { name: "black", value: "#252a33" }
-  ]
-});
-
 stories.add(
-  "Gradient",
+  "CSS Gradient",
   () => {
     const title = text("Title", "St. Louis, MO");
     const subtitle = text("Subtitle", "Destination");
 
     return html`
+      <chameleon-hero title="${title}" subTitle="${subtitle}"> </chameleon-hero>
+    `;
+  },
+  { info: { inline: true } }
+);
+
+stories.add(
+  "Background Image",
+  () => {
+    const title = text("Title", "Washington, DC");
+    const subtitle = text("Subtitle", "Destination");
+    const imageUrl = text("Image URL", "https://picsum.photos/id/122/2100/856");
+
+    return html`
       <chameleon-hero
         title="${title}"
         subTitle="${subtitle}"
-        backgroundImageUrl="https://images.pexels.com/photos/1363876/pexels-photo-1363876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        backgroundImageUrl="${imageUrl}"
       >
-        Thshshhshshshs
       </chameleon-hero>
     `;
   },
