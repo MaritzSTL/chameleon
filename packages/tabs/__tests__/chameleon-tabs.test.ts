4506e0e622d210f14b2938e8af374915591f93f4
Ryan Coleman <me@ryuhhnn.com>
2019-10-10 00:29:07 -0500

    fixes broken test

--- packages/tabs/__tests__/chameleon-tabs.test.ts
+++ packages/tabs/__tests__/chameleon-tabs.test.ts
@@ -1,10 +1,11 @@
 import { fixture, html, expect } from "@open-wc/testing";
 import "../src/chameleon-tabs";
+import "../src/chameleon-tab";
 
 describe("chameleon-tabs", () => {
   it("renders", async () => {
     const el = await fixture(html`
-      <chameleon-tabs></chameleon-tabs>
+      <chameleon-tabs><chameleon-tab></chameleon-tab></chameleon-tabs>
     `);
     expect(Boolean(el.shadowRoot)).to.equal(true);
   });
