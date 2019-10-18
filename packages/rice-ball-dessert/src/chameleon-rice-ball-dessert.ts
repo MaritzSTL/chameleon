75c0090f978a942b581ffebd8b5037c9b0acb6fe
Mandy Michel <amanda.michel@gmail.com>
2019-10-18 13:03:40 -0500

    made it so you can change the rice ball color because I just had to

--- packages/rice-ball-dessert/src/chameleon-rice-ball-dessert.ts
+++ packages/rice-ball-dessert/src/chameleon-rice-ball-dessert.ts
@@ -3,12 +3,19 @@ import {
   css,
   TemplateResult,
   customElement,
-  html
+  html,
+  property
 } from "lit-element";
 import { styles as riceBallStyles } from "./chameleon-rice-ball-dessert-styles";
 
 @customElement("chameleon-rice-ball-dessert")
 export default class ChameleonCard extends LitElement {
+  /**
+   * Properties
+   */
+  // Rice ball color
+  @property({ type: String })
+  riceBallColor = "red";
   /**
    * Styles
    */
@@ -26,13 +33,13 @@ export default class ChameleonCard extends LitElement {
         </div>
         <div class="contents">
           <div class="syrup"></div>
-          <div class="riceball uno">
+          <div class="riceball uno" style="background: ${this.riceBallColor}">
             <div class="face"></div>
           </div>
-          <div class="riceball dos">
+          <div class="riceball dos" style="background: ${this.riceBallColor}">
             <div class="face"></div>
           </div>
-          <div class="riceball tres">
+          <div class="riceball tres" style="background: ${this.riceBallColor}">
             <div class="face"></div>
           </div>
         </div>
