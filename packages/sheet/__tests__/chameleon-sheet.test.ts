a500e5c7ce1fe8b849d45a4e4d9459e642f4d30f
Krelo <netlocolerk16@hotmail.com>
2019-10-22 11:36:21 -0500

    test fix

--- packages/sheet/__tests__/chameleon-sheet.test.ts
+++ packages/sheet/__tests__/chameleon-sheet.test.ts
@@ -1,26 +1,11 @@
-import { html, expect } from "@open-wc/testing";
-import "./mtzwc-sheet.js";
-import { litFixture } from "@open-wc/testing-helpers";
-import sinon from "sinon";
+import { fixture, html, expect } from "@open-wc/testing";
+import "./chameleon-sheet.js";
 
-const fixture = html`
-  <mtzwc-sheet></mtzwc-sheet>
-`;
-
-describe("mtzwc-sheet", () => {
-  let element;
-
-  beforeEach(async () => {
-    element = await litFixture(fixture);
-  });
-
-  it("should have tag name defined", () => {
-    expect(element.tagName.toLowerCase()).to.equal("mtzwc-sheet");
-  });
-
-  it("sets default values in constructor", () => {
-    expect(element.header).to.equal("");
-    expect(element.subHeader).to.equal("");
-    expect(element.sheetOpened).to.be.false;
+describe("chameleon-sheet", () => {
+  it("renders", async () => {
+    const el = await fixture(html`
+      <chameleon-sheet></chameleon-sheet>
+    `);
+    expect(Boolean(el.shadowRoot)).to.equal(true);
   });
 });
