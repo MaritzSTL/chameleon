75932ee0ea1cc984ce8accfceb5ba919a39c8318
Ryan Coleman <me@ryuhhnn.com>
2019-10-23 18:29:18 -0500

    adds in testing coverage for chameleon-textarea, closes #57

--- packages/textarea/__tests__/chameleon-textarea.test.ts
+++ packages/textarea/__tests__/chameleon-textarea.test.ts
@@ -1,11 +1,66 @@
-import { fixture, html, expect } from "@open-wc/testing";
+import { litFixture, html, expect } from "@open-wc/testing";
+import sinon from "sinon";
 import "../src/chameleon-textarea";
 
+const fixture = html`
+  <chameleon-textarea></chameleon-textarea>
+`;
+
 describe("chameleon-textarea", () => {
+  let element;
+
+  beforeEach(async () => {
+    element = await litFixture(fixture);
+  });
+
   it("renders", async () => {
-    const el = await fixture(html`
-      <chameleon-textarea></chameleon-textarea>
-    `);
-    expect(Boolean(el.shadowRoot)).to.equal(true);
+    expect(Boolean(element.shadowRoot)).to.equal(true);
+  });
+
+  it("returns _el", () => {
+    const el = element._el;
+    expect(el.constructor.name).to.equal("HTMLTextAreaElement");
+  });
+
+  it("renders labelText", () => {
+    element.label = "chameleon";
+    expect(element.labelText.constructor.name).to.equal("TemplateResult");
+  });
+
+  it("renders errorText", () => {
+    element.validationMessage = "chameleon";
+    expect(element.errorText.constructor.name).to.equal("TemplateResult");
+  });
+
+  it("returns validity", () => {
+    expect(element.validity.constructor.name).to.equal("ValidityState");
+  });
+
+  it("willValidate returns a boolean", () => {
+    expect(typeof element.willValidate).to.equal("boolean");
+  });
+
+  it("checkValidity returns a boolean", () => {
+    expect(typeof element.checkValidity()).to.equal("boolean");
+  });
+
+  it("_handleBlur calls checkValidity", () => {
+    // TBH I'm not convinced this test is actually behaving expectedly...
+    const checkValidity = sinon.spy(element, "checkValidity");
+    const handleBlur = sinon.spy(element, "_handleBlur");
+
+    element._handleBlur();
+
+    expect(checkValidity.calledImmediatelyAfter(handleBlur));
+  });
+
+  it("_handleInvalid sets validationMessage", () => {
+    element._handleInvalid();
+    expect(element.validationMessage).to.equal(element._el.validationMessage);
+  });
+
+  it("handles input", () => {
+    element._handleInput({ target: { value: "chameleon" } });
+    expect(element.value).to.equal("chameleon");
   });
 });
