4d3fc014d0672f27529e0be6ea32f5fde4f8fbb0
Ryan Coleman <me@ryuhhnn.com>
2019-10-24 10:43:36 -0500

    fixes broken ci build for chameleon-card-footer

--- packages/card-footer/__tests__/chameleon-card-footer.test.ts
+++ packages/card-footer/__tests__/chameleon-card-footer.test.ts
@@ -1,9 +1,9 @@
-import { fixture, html, expect } from "@open-wc/testing";
+import { litFixture, html, expect } from "@open-wc/testing";
 import "../src/chameleon-card-footer";
 
 describe("chameleon-card-footer", () => {
   it("renders", async () => {
-    const el = await fixture(html`
+    const el = await litFixture(html`
       <chameleon-card-footer></chameleon-card-footer>
     `);
     expect(Boolean(el.shadowRoot)).to.equal(true);
