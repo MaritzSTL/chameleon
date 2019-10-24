0ac3cf10b614ff8067d908a623092e3ba768f9c5
Ryan Coleman <me@ryuhhnn.com>
2019-10-24 13:25:37 -0500

    adding more tabs tests

--- packages/tabs/__tests__/chameleon-tabs.test.ts
+++ packages/tabs/__tests__/chameleon-tabs.test.ts
@@ -1,12 +1,35 @@
-import { fixture, html, expect } from "@open-wc/testing";
+import { litFixture, html, expect } from "@open-wc/testing";
+import sinon from "sinon";
+import ChameleonTabs from "../src/chameleon-tabs";
 import "../src/chameleon-tabs";
 import "../src/chameleon-tab";
 
+const fixture = html`
+  <chameleon-tabs><chameleon-tab></chameleon-tab></chameleon-tabs>
+`;
+
 describe("chameleon-tabs", () => {
-  it("renders", async () => {
-    const el = await fixture(html`
-      <chameleon-tabs><chameleon-tab></chameleon-tab></chameleon-tabs>
-    `);
-    expect(Boolean(el.shadowRoot)).to.equal(true);
+  let element;
+
+  beforeEach(async () => {
+    element = await litFixture(fixture);
   });
+
+  it("renders", async () => {
+    expect(Boolean(element.shadowRoot)).to.equal(true);
+  });
+
+  it("dispatches an event on handleToggle", () => {
+    const tab = element.querySelector("chameleon-tab");
+    const spy = sinon.spy(tab, "dispatchEvent");
+
+    tab.handleToggle();
+
+    expect(spy).to.be.calledOnce;
+  });
+
+  // it("throws error if no tabs are given", () => {
+  //   const tabs = () => new ChameleonTabs();
+  //   expect(tabs()).to.throw();
+  // });
 });
