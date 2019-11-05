3f583b423d3c153c27a827bbd47ce8317196b18c
Ryan Coleman <me@ryuhhnn.com>
2019-11-05 17:53:39 -0600

    updates all modules to output builds into src/

--- packages/rice-ball-dessert/__tests__/chameleon-rice-ball-dessert.test.ts
+++ packages/rice-ball-dessert/__tests__/chameleon-rice-ball-dessert.test.ts
@@ -1,5 +1,5 @@
 import { fixture, html, expect } from "@open-wc/testing";
-import "../src/chameleon-rice-ball-dessert";
+import "@chameleon-ds/rice-ball-dessert/src/chameleon-rice-ball-dessert";
 
 describe("chameleon-rice-ball-dessert", () => {
   it("renders", async () => {
