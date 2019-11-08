8b94facc7671b3cdb766ae1cfd397607b05f5852
Ryan Coleman <me@ryuhhnn.com>
2019-11-08 10:54:54 -0600

    finishes up the multiselect component

--- .storybook/stories/multiselect.stories.ts
+++ .storybook/stories/multiselect.stories.ts
@@ -1,30 +1,30 @@
 import { storiesOf } from "@storybook/polymer";
 import { withKnobs, boolean, text } from "@storybook/addon-knobs";
-import { html } from "lit-html";
+import { html, svg } from "lit-html";
 import "@chameleon-ds/multiselect/src/chameleon-multiselect";
 
 const stories = storiesOf("Multiselect", module);
 
 stories.addDecorator(withKnobs);
 
-// const searchIcon = (slot: string) => svg`
-//   <svg
-//     xmlns="http://www.w3.org/2000/svg"
-//     width="24"
-//     height="24"
-//     viewBox="0 0 24 24"
-//     fill="none"
-//     stroke="currentColor"
-//     stroke-width="2"
-//     stroke-linecap="round"
-//     stroke-linejoin="round"
-//     class="feather feather-search"
-//     slot="${slot}"
-//   >
-//     <circle cx="11" cy="11" r="8"></circle>
-//     <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-//   </svg>
-// `;
+const searchIcon = (slot: string) => svg`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-search"
+    slot="${slot}"
+  >
+    <circle cx="11" cy="11" r="8"></circle>
+    <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+  </svg>
+`;
 
 const options = [
   {
@@ -94,8 +94,29 @@ stories.add(
         .label="${label}"
         .placeholder="${placeholder}"
         .options="${options}"
-        .values="${["arch"]}"
+        .value="${["arch"]}"
       ></chameleon-multiselect>
+    `;
+  },
+  { info: { inline: true }, options: { enableShortcuts: false } }
+);
+
+stories.add(
+  "With Icon",
+  () => {
+    const disabled = boolean("Disabled", false);
+    const label = text("Label", "");
+    const placeholder = text("Placeholder", "Select an option...");
+
+    return html`
+      <chameleon-multiselect
+        ?disabled="${disabled}"
+        .label="${label}"
+        .placeholder="${placeholder}"
+        .options="${options}"
+      >
+        ${searchIcon("icon")}
+      </chameleon-multiselect>
     `;
   },
   { info: { inline: true }, options: { enableShortcuts: false } }
