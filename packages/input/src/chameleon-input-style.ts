027f2707ace4e1cb84315ab455e782fb112cbf6f
Ryan Coleman <me@ryuhhnn.com>
2019-11-11 01:41:40 -0600

    fixes issue where icon spills out of input field

--- packages/input/src/chameleon-input-style.ts
+++ packages/input/src/chameleon-input-style.ts
@@ -16,7 +16,7 @@ export default css`
     box-sizing: border-box;
     font-size: var(--font-size-input, 0.938rem);
     font-family: var(--font-family, sans-serif);
-    max-width: 100%;
+    width: 100%;
     padding: var(--input-padding, 0.625rem 0.5rem);
   }
 
