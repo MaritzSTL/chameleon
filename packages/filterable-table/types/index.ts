e4312ae4ec9ba653b20f19d9c0b2f50666de314c
Keith Walter <keith.walter@maritz.com>
2019-11-13 17:49:37 -0600

    rename type defintions file

---
+++ packages/filterable-table/types/index.ts
@@ -0,0 +1,44 @@
+import { TemplateResult } from "lit-element";
+
+export type Columns = Column[];
+
+export type Column = {
+  header: string;
+  row: (row: Row) => TemplateResult;
+  detailsRow?: (detailsRow: Row, row: Row) => TemplateResult;
+  filter?: {
+    name?: string;
+    items?: FilterItems;
+  };
+  sortable?: boolean;
+  searchable?: boolean;
+};
+
+export type Rows = Row[];
+
+export type Row = {
+  showDetails?: boolean;
+  details?: Row[];
+} & {
+  [key: string]: string | string[];
+};
+
+export type FilterItems = FilterItem[];
+
+export type FilterItem = {
+  value: string;
+  label: string;
+};
+
+export type Filters = {
+  [key: string]: string | string[];
+};
+
+export type ChangeCustomEvent = {
+  detail: {
+    filters: Filters;
+    page: number;
+  };
+  bubbles: true;
+  composed: true;
+};
