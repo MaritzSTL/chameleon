885ddea1a59a8be2292a2ec3dd3381a418b07003
shawn b <shawn.barnes@maritz.com>
2019-11-21 12:01:22 -0600

    removed theme/base import from dialog

--- packages/dialog/src/chameleon-dialog.ts
+++ packages/dialog/src/chameleon-dialog.ts
@@ -7,7 +7,6 @@ import {
 } from "lit-element";
 import { nothing, svg, SVGTemplateResult } from "lit-html";
 import { classMap } from "lit-html/directives/class-map";
-import base from "@chameleon-ds/theme/base";
 import style from "./chameleon-dialog-style";
 import "@chameleon-ds/card/src/chameleon-card";
 import "@chameleon-ds/button/src/chameleon-button";
