8e4f0a6fc145b33711ab3cf3036b1a38836d5043
Connie <cforth4@gmail.com>
2019-11-25 13:58:19 -0600

    changed background when no image is provided

--- packages/card-image/src/chameleon-card-image.ts
+++ packages/card-image/src/chameleon-card-image.ts
@@ -7,6 +7,7 @@ import {
   PropertyValues
 } from "lit-element";
 import style from "./chameleon-card-image-style";
+import { nothing } from "lit-html";
 
 @customElement("chameleon-card-image")
 export default class ChameleonCardImage extends LitElement {
@@ -22,6 +23,9 @@ export default class ChameleonCardImage extends LitElement {
   @property({ type: String })
   alt = "";
 
+  @property({ type: Boolean })
+  hasImage = true;
+
   /**
    * Styles
    */
@@ -33,16 +37,30 @@ export default class ChameleonCardImage extends LitElement {
    */
   render(): TemplateResult {
     return html`
-      <img src="${this.src}" alt="${this.alt}" />
+      ${this.hasImageProps}
+      <slot></slot>
     `;
   }
-  updated(changedProperties: PropertyValues) {
-  if (
-    changedProperties.has("src") && 
-   this.src === "" &&
-   this.src !== undefined
-    ) {
-    this.style.backgroundColor = "green"
-   }
 
+  get hasImageProps(): TemplateResult | object {
+    return this.hasImage !== false
+      ? html`
+        <img src="${this.src}" alt="${this.alt}"</div>
+      `
+      : nothing;
+  }
+
+  updated(changedProperties: PropertyValues) {
+    if (
+      changedProperties.has("src") &&
+      this.src === "" &&
+      this.src !== undefined
+    ) {
+      this.hasImage = false;
+      this.style.backgroundImage =
+        "linear-gradient(to top, var(--color-primary-dark, #004587), var(--color-primary-light, #679dea))";
+      this.style.borderRadius = "var(--border-radius, 0.5rem)";
+      this.style.height = "100px";
+    }
+  }
 }
