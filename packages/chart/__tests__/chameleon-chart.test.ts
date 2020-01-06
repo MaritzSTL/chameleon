daf3229554ac6d406952f8bc219f32cfa585e3c6
rodrigomartinezh <rodrigo.martinezh@maritz.com>
2020-01-06 17:13:31 -0600

    chart-chameleon
    add some init test
    
    closes #SV-6348

--- packages/chart/__tests__/chameleon-chart.test.ts
+++ packages/chart/__tests__/chameleon-chart.test.ts
@@ -1,11 +1,26 @@
-import { fixture, html, expect } from "@open-wc/testing";
+import { litFixture, html, expect } from "@open-wc/testing";
 import "@chameleon-ds/chart/src/chameleon-chart";
 
+const fixture = html`
+  <chameleon-chart></chameleon-chart>
+`;
+
 describe("chameleon-chart", () => {
+  let element;
+
+  beforeEach(async () => {
+    element = await litFixture(fixture);
+  });
+
   it("renders", async () => {
-    const el = await fixture(html`
-      <chameleon-chart></chameleon-chart>
-    `);
-    expect(Boolean(el.shadowRoot)).to.equal(true);
+    expect(Boolean(element.shadowRoot)).to.equal(true);
+  });
+
+  it("convert percentage to degrees", async () => {
+    const percentage = 10;
+
+    const degrees = element._percentageToDegrees(percentage);
+    const expectedDegrees = -96;
+    expect(degrees).to.be.equal(expectedDegrees);
   });
 });
