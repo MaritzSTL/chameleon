c470074949372960cc4731b202cce36f0dc7dd2a
Chuck Housley <charles.housley@maritz.com>
2020-04-06 10:17:13 -0500

    Clean up webpack alias block

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -6,28 +6,23 @@ module.exports = ({ config }: { config: webpack.Configuration }) => {
     exclude: /node_modules/,
     use: [
       {
-        loader: require.resolve("awesome-typescript-loader")
-      }
-    ]
+        loader: require.resolve("awesome-typescript-loader"),
+      },
+    ],
   });
   config!.resolve!.extensions!.push(".ts");
-  config!.resolve!.alias!["@chameleon-ds/loader"] =
-    "@chameleon-ds/loader/src/chameleon-loader";
-  config!.resolve!.alias!["@chameleon-ds/button"] =
-    "@chameleon-ds/button/src/chameleon-button";
-  config!.resolve!.alias!["@chameleon-ds/skeleton"] =
-    "@chameleon-ds/skeleton/src/chameleon-skeleton";
-  config!.resolve!.alias!["@chameleon-ds/chip"] =
-    "@chameleon-ds/chip/src/chameleon-chip";
-  config!.resolve!.alias!["@chameleon-ds/select"] =
-    "@chameleon-ds/select/src/chameleon-select";
-  config!.resolve!.alias!["@chameleon-ds/card"] =
-    "@chameleon-ds/card/src/chameleon-card";
-  config!.resolve!.alias!["@chameleon-ds/input"] =
-    "@chameleon-ds/input/src/chameleon-input";
-  config!.resolve!.alias!["@chameleon-ds/multiselect"] =
-    "@chameleon-ds/multiselect/src/chameleon-multiselect";
-  config!.resolve!.alias!["@chameleon-ds/paginator"] =
-    "@chameleon-ds/paginator/src/chameleon-paginator";
+  Object.assign(config!.resolve!.alias!, {
+    "@chameleon-ds/loader": "@chameleon-ds/loader/src/chameleon-loader",
+    "@chameleon-ds/button": "@chameleon-ds/button/src/chameleon-button",
+    "@chameleon-ds/skeleton": "@chameleon-ds/skeleton/src/chameleon-skeleton",
+    "@chameleon-ds/chip": "@chameleon-ds/chip/src/chameleon-chip",
+    "@chameleon-ds/select": "@chameleon-ds/select/src/chameleon-select",
+    "@chameleon-ds/card": "@chameleon-ds/card/src/chameleon-card",
+    "@chameleon-ds/input": "@chameleon-ds/input/src/chameleon-input",
+    "@chameleon-ds/multiselect":
+      "@chameleon-ds/multiselect/src/chameleon-multiselect",
+    "@chameleon-ds/paginator":
+      "@chameleon-ds/paginator/src/chameleon-paginator",
+  });
   return config;
 };
