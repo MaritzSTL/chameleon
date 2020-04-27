ce45a6ef199519afb6ea999770fcddfabd528c98
Mandy Michel <amanda.michel@gmail.com>
2020-04-27 15:20:46 -0500

    adding test

--- packages/skeleton/__tests__/chameleon-skeleton.test.ts
+++ packages/skeleton/__tests__/chameleon-skeleton.test.ts
@@ -3,9 +3,13 @@ import "../src/chameleon-skeleton";
 
 describe("chameleon-skeleton", () => {
   it("renders", async () => {
-    const el = await fixture(html`
-      <chameleon-skeleton></chameleon-skeleton>
-    `);
+    const el = await fixture(html` <chameleon-skeleton></chameleon-skeleton> `);
     expect(Boolean(el.shadowRoot)).to.equal(true);
+  });
+  it("returns a radius half the size of the circle height if theme is secondary", async () => {
+    const el = await fixture(html` <chameleon-skeleton></chameleon-skeleton> `);
+    el.theme = "secondary";
+    el.cHeight = 50;
+    expect(el.circleRadius).to.eql(25);
   });
 });
