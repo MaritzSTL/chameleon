fda324d2fe5ebae027ee5b487938e32ae72a84ff
Chris Zempel <chris.m.zempel@gmail.com>
2020-05-28 17:04:27 -0500

    refactor(sheet): now removes itself properly during standard mounting strategy

--- packages/sheet/src/chameleon-sheet.ts
+++ packages/sheet/src/chameleon-sheet.ts
@@ -11,6 +11,7 @@ import "@chameleon-ds/button";
 
 export default class ChameleonSheet extends LitElement {
   private static _defaultCloseIcon: SVGTemplateResult = svg`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
+  private _mount?: HTMLElement;
 
   /**
    * Properties
@@ -27,7 +28,7 @@ export default class ChameleonSheet extends LitElement {
 
   constructor() {
     super();
-    this.addEventListener("click", this._dataBehaviorClose);
+    this.renderRoot.addEventListener("click", this._dataBehaviorClose);
   }
 
   createRenderRoot(): ShadowRoot {
@@ -40,13 +41,12 @@ export default class ChameleonSheet extends LitElement {
     }
 
     // Place on document body so this is a "top-level" element.
-    const mount = document.createElement("span");
-    document.body.appendChild(mount);
-    return mount.attachShadow({ mode: "open" });
+    this._mount = document.createElement("span");
+    document.body.appendChild(this._mount);
+    return this._mount.attachShadow({ mode: "open" });
   }
 
   updated(changedProperties: PropertyValues): void {
-    debugger;
     if (changedProperties.has("opened")) {
       this.dispatchEvent(
         new CustomEvent("sheetchange", {
@@ -60,7 +60,9 @@ export default class ChameleonSheet extends LitElement {
   disconnectedCallback() {
     super.disconnectedCallback();
     this.removeEventListener("click", this._dataBehaviorClose);
-    this.parentElement?.removeChild(this);
+    if (this._mount) {
+      this._mount.parentElement?.removeChild(this._mount);
+    }
   }
 
   open(): void {
